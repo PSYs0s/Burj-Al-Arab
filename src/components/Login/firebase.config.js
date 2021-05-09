61b3ee022ccf924ae179a109f90f4135de7667c3
A. S. M. Toha <71426658+PSYs0s@users.noreply.github.com>
2021-05-09 15:51:35 +0600

    delet config

--- src/components/Login/firebase.config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyC9raNfuVZbfumVw1VDcAYTDmnZSrYuEds",
-    authDomain: "burj-al-arab-49421.firebaseapp.com",
-    projectId: "burj-al-arab-49421",
-    storageBucket: "burj-al-arab-49421.appspot.com",
-    messagingSenderId: "895413261666",
-    appId: "1:895413261666:web:fb3fbe589cbcc7589e883b"
-  };
-  export default firebaseConfig
